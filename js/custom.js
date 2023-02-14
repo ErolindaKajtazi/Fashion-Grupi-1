ebf1a301adee57cae8a0138e661f923f7314d329
Erolinda Kajtazi <erolindakajtazi@gmail.com>
2023-02-14 13:49:56 +0100

    first commit

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+//Responsive navbar
+let navButton = document.querySelector(".mobile-nav");
+let navHeader = document.querySelector(".header");
+
+console.log(navButton);
+console.log(navHeader);
+
+navButton.addEventListener("click", function () {
+    navHeader.classList.toggle("open-nav");
+});
+
+
