cb1569ef2a4a29314218604e1e42a6832ee8a864
Erolinda Kajtazi <erolindakajtazi@gmail.com>
2023-02-16 17:53:35 +0100

    ...

--- js/custom.js
+++ js/custom.js
@@ -9,3 +9,31 @@ navButton.addEventListener("click", function () {
     navHeader.classList.toggle("open-nav");
 });
 
+// Get the form element and the input fields
+const form = document.querySelector('.login-form');
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+
+// Function to validate the form
+function validateForm() {
+  if (usernameInput.value.trim() === '') {
+    usernameInput.style.borderBottomColor = 'red';
+  } else {
+    usernameInput.style.borderBottomColor = '';
+  }
+
+  if (passwordInput.value.trim() === '') {
+    passwordInput.style.borderBottomColor = 'red';
+  } else {
+    passwordInput.style.borderBottomColor = '';
+  }
+
+  if (form.checkValidity()) {
+    // Submit the form
+    form.submit();
+  }
+}
+
+// Add an event listener to the Login button
+const loginButton = document.querySelector('.btn-log');
+loginButton.addEventListener('click', validateForm);
